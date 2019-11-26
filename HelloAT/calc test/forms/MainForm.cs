873543fe44bce46a33ac42dc5b6f02048169132e
nanurev <kamikami11@yandex.ru>
2019-11-27 02:00:57 +0300

    vs 2015. Hello Page Object

---
+++ HelloAT/calc test/forms/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using NUnit.Framework;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.AutomationElements.Infrastructure;
+using FlaUI.Core.AutomationElements.PatternElements;
+
+namespace HelloAT.calc_test
+{
+    class MainForm  //Главное окно
+    {
+
+        public Window window { get; set; }
+        public AutomationElement panel { get; set; }
+        public Button button0 { get; set; }
+
+        public Button button1 { get; set; }
+        public Button button2 { get; set; }
+        public Button button3 { get; set; }
+        public Button button4 { get; set; }
+        public Button button5 { get; set; }
+        public Button button6 { get; set; }
+        public Button button7 { get; set; }
+        public Button button8 { get; set; }
+        public Button button9 { get; set; }
+
+        public Button buttonPlus { get; set; }
+        public Button buttonMultiply { get; set; }
+        public Button buttonEqual { get; set; }
+
+        public Label textResult { get; set; }//Результат
+
+
+
+        public MainForm (Window window)
+        {
+            this.window = window;
+            this.window.Focus(); //окно на передний план
+
+            panel = window.FindFirstByXPath($"//Pane");
+            button0 = panel.FindFirstByXPath($"//Button[@Name=0]").AsButton();
+            button1 = panel.FindFirstByXPath($"//Button[@Name=1]").AsButton();
+            button2 = panel.FindFirstByXPath($"//Button[@Name=2]").AsButton();
+            button3 = panel.FindFirstByXPath($"//Button[@Name=3]").AsButton();
+            button4 = panel.FindFirstByXPath($"//Button[@Name=4]").AsButton();
+            button5 = panel.FindFirstByXPath($"//Button[@Name=5]").AsButton();
+            button6 = panel.FindFirstByXPath($"//Button[@Name=6]").AsButton();
+            button7 = panel.FindFirstByXPath($"//Button[@Name=7]").AsButton();
+            button8 = panel.FindFirstByXPath($"//Button[@Name=8]").AsButton();
+            button9 = panel.FindFirstByXPath($"//Button[@Name=9]").AsButton();
+
+            buttonPlus = panel.FindFirstByXPath($"//Button[@Name='Сложение']").AsButton();
+            buttonMultiply = panel.FindFirstByXPath($"//Button[@Name='Умножение']").AsButton();
+            buttonEqual = panel.FindFirstByXPath($"//Button[@Name='Равно']").AsButton();
+
+            textResult = panel.FindFirstByXPath($"//Text[@AutomationId='158']").AsLabel();//Результат
+        }
+
+        
+
+
+    }
+}
---
+++ HelloAT/calc test/test/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using FlaUI.Core;
+using FlaUI.UIA3;
+using NUnit.Framework;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
+
+using HelloAT.calc_test;
+
+
+namespace HelloAT
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void CalcAttachWithAbsoluteExePath()
+        {
+            using (Application.Attach("calc.exe"))
+            {
+                using (var app = Application./*Launch*/Attach(@"C:\WINDOWS\system32\calc.exe"))
+                {
+                    using (var automation = new UIA3Automation())
+                    {
+                        Window window = app.GetMainWindow(automation);
+                        MainForm mainForm = new MainForm(window);
+
+                        //window.Focus(); //окно на передний план
+
+                        /*
+                        var panel = window.FindFirstByXPath($"//Pane");
+                        Button button1 = panel.FindFirstByXPath($"//Button[@Name=1]").AsButton();
+                        Button button2 = panel.FindFirstByXPath($"//Button[@Name=2]").AsButton();
+                        Button button3 = panel.FindFirstByXPath($"//Button[@Name=3]").AsButton();
+
+                        Button buttonPlus = panel.FindFirstByXPath($"//Button[@Name='Сложение']").AsButton();
+                        Button buttonMultiply = panel.FindFirstByXPath($"//Button[@Name='Умножение']").AsButton();
+                        Button buttonEqual = panel.FindFirstByXPath($"//Button[@Name='Равно']").AsButton();
+
+                        Label textResult = panel.FindFirstByXPath($"//Text[@AutomationId='158']").AsLabel();//Результат
+
+                        button1.Click();
+                        buttonPlus.Click();
+                        button2.Click();
+                        buttonMultiply.Click();
+                        button3.Click();
+                        buttonEqual.Click();
+                        */
+
+                        mainForm.button1.Click();
+                        mainForm.buttonPlus.Click();
+                        mainForm.button2.Click();
+                        mainForm.buttonMultiply.Click();
+                        mainForm.button3.Click();
+                        mainForm.buttonEqual.Click();
+
+
+                        Assert.That(mainForm.textResult.Text.Equals("9"));
+
+                        //app.WaitWhileMainHandleIsMissing();
+                        //app.Close(); //Закрыть окно
+
+
+                        //Assert.That(window.Title, Is.Not.Null);
+
+
+                    }
+                }
+            }
+        }
+    }
+}
